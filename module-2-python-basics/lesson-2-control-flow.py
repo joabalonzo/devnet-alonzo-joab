31fa8871d2cfc5fca23e02c741ac6983703928c8
joabalonzo <joabalonzo3@gmail.com>
2026-09-25 22:25:03 +0800

    Fourth lesson-2-control-flow.py

--- module-2-python-basics/lesson-2-control-flow.py
+++ module-2-python-basics/lesson-2-control-flow.py
@@ -1,22 +1,52 @@
 """
 Module 2 — Lesson 2: Control Flow (if / elif / else)
-Student: [your name]
-Date: [date]
+Student: [Alonzo, Joab P.]
+Date: [September 25, 2026]
 
 ============================================
 WHAT IS THIS TOPIC? (explain it like you're
 teaching a friend who's never coded before)
 ============================================
-[write your own explanation here]
+[Control flow is used to tell a program what to do depending on the situation.
+In our everyday life, we always make a decision.
+
+For example: 
+If i got home early. I will play basketball.
+If i got home late. I will take a rest.
+
+Python can also make a decision by using if, elif, and else.
+The "If" statement checks the condition, if the condition is true,
+python will run the code in side the if statement.
+
+The "elif" statement means "else if".
+It is used when we want to check another condition.
+
+The "else" statement is used when none of the previous conditions are true.
+
+This conditional statement allows the program to make a decision instead of doing 
+it again and again.]
 
 
 ============================================
 KEY VOCABULARY
 ============================================
-- condition:
-- if / elif / else:
-- comparison operator:
-- boolean expression:
+- condition: This is a situation that python checks to see if it is true or false.
+- if / elif / else: If used to check the first condition.
+elif means else if it checks another condition when the previous condition is false.
+- comparison operator: A symbol used to compare two values.
+- boolean expression: An expression that result is either true or false.
+- >:
+  Greater than.
+- <:
+  Less than.
+- ==:
+  Equal to.
+- >=:
+  Greater than or equal to.
+- <=:
+  Less than or equal to.
+- !=:
+  Not equal to.
 (add more as needed)
 
 
@@ -26,16 +56,32 @@ MY OWN EXAMPLE(S)
 Write at least one working example below that you
 came up with yourself — not copied from class.
 """
+#My example is simple program based on what i would do when i get home. I used this simple example for us to understand it easily.
 
-# --- your code example goes here ---
+time = "early"
+
+if time == "early":
+    print("I will play basketball.")
+elif time == "late":
+    print("I will take a rest.")
+else:
+    print("I will decide what to do.")
 
 
 """
 ============================================
 A MISTAKE I MADE (or one I want to avoid)
 ============================================
-[what's something confusing or easy to get wrong
-about this topic?]
+[One mistake i want to avoid is getting confused in = and ==.
+i use = when i want to store value in a variable, while == is use to 
+check if the two values are equal.
+For example:
+
+arrival = "early" stores "early" in the variable.
+
+arrival == "early" checks if the value is "early".
+
+I also need to always remember that putting a colon after if, elif, and else is important.]
 
 
 ============================================
